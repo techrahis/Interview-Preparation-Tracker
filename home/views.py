d5f335f4dc603d07e4aefd98bcf97f868989e8ff
Rajarshi Samaddar <itsrajarshi@gmail.com>
2022-06-20 21:06:08 +0530

    update

---
+++ home/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
