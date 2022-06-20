c46fd7c0b1e6890853eb0731f51089615ecc9b32
soumayadipsaha <soumayadipsaha2002@gmail.com>
2022-06-21 01:23:44 +0530

    home update

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template import loader
+from home.models import Topic, Question
 
 
 def home(request):
+    topic = Topic.objects.all()
+    context = {
+        'topic': topic,
+    }
     template = loader.get_template('home.html')
+    return HttpResponse(template.render(context, request))
+
+def question(request, slug):
+    template = loader.get_template('question.html')
     return HttpResponse(template.render())
